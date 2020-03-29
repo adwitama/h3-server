8329285eae7b16b7ebd48c306df0808cd9133bd9
Adhitya <adhitya@Adhityas-MacBook-Pro.local>
2020-03-29 10:53:39 +0700

    Add CORS support

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const h3 = require('h3-js')
+const cors = require('cors')
 
 const app = express()
 const port = 3300
+
+app.use(cors())
 
 app.get("/h3/geo/:hexId", (req, res, next) => {
  const hexId = req.params.hexId
